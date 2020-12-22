153e269de4314b393ce7d8671c2eca38562255f9
guohu01 <1610907559@qq.com>
2020-12-22 18:12:17 +0800

    12月22日

--- src/pages/details/detail_main.vue
+++ src/pages/details/detail_main.vue
@@ -61,102 +61,39 @@
         <!-- 留言区域-->
         <div>
           <div id="messageboard">
-            <el-form ref="form" label-width="50px">
-              <el-form-item label="标题">
-                <el-input v-model="title" placeholder="请输入标题"></el-input>
-              </el-form-item>
-              <el-form-item label="内容">
-                <el-input v-model="content" placeholder="请输入内容"></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button @click="add()">添加</el-button>
-                <el-button @click="handleReset">重置</el-button>
-              </el-form-item>
-              <el-table border :data="mydata">
-                <el-table-column label="编号" inline-template :context="_self">
-                  <span>{{$index+1}}</span>
-                </el-table-column>
-                <el-table-column prop="title" label="标题">
-                </el-table-column>
-                <el-table-column prop="content" label="内容">
-                </el-table-column>
-                <el-table-column label="操作" inline-template :context="_self">
-                  <span>
-                    <el-button size="small" @click="showDialog()">删除</el-button>
-                  </span>
-                </el-table-column>
-              </el-table>
-              <div style="text-align:right" v-show="mydata.length>0">
-                <el-button size="small" @click="showDelallDialog()">全部删除</el-button>
-              </div>
-            </el-form>
-
-            <el-dialog title="提示" v-model="dialogVisible" size="tiny">
-              <span v-if="nowIndex==-2">删除全部条留言</span>
-              <span v-else>删除此条留言</span>
-              <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="del(nowIndex)">确 定</el-button>
-              </span>
-            </el-dialog>
+            <!-- <ArticleComment></ArticleComment> -->
+            <comment :comments="commentData"></comment>
           </div>
         </div>
       </div>
     </el-col>
     <el-col :md="{span:6}">
       <div style="background-color: #fff;">
-        asda
+        目录
       </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
+  import ArticleComment from '../main/ArticleComment.vue'
+  import * as CommentData from '../../public/js/mockdata'
+  import Comment from '../main/comment.vue'
+
   export default {
     data() {
       return {
         fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
         url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-        title: '',
-        content: '',
-        mydata: [],
-        dialogVisible: false,
-        nowIndex: -100,
+        commentData: []
       }
     },
-    methods: {
-      add() {
-        if (this.title == '' || this.content == '') {
-          this.$message.error('请填写完整');
-        } else {
-          this.mydata.push({
-            title: this.title,
-            content: this.content,
-          });
-          this.title = '';
-          this.content = '';
-        }
-      },
-      showDialog() {
-        this.dialogVisible = true;
-      },
-      showDelallDialog() {
-        this.dialogVisible = true;
-        this.nowIndex = -2;
-      },
-      del(n) {
-        if (n == -2) {
-          this.mydata = [];
-        } else {
-          this.mydata.splice(n, 1);
-        }
-        this.nowIndex = -100;
-        this.dialogVisible = false;
-      },
-      handleReset() {
-        this.title = '';
-        this.content = '';
-      }
+    created() {
+      this.commentData = CommentData.comment.data;
+    },
+    components: {
+      ArticleComment,
+      Comment
     },
   }
 </script>
